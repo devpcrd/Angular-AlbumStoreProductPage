c5103b8eeaf4909ed8227c88a65b6319b5d7dd6c
NAME <mail@gmail.com>
2021-12-22 11:49:58 +0100

    This is the end of part3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
-import 'rxjs/add/operator /map';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -9,10 +9,8 @@ export class ProductService {
   private _albumUrl = '../assets/album.json';
 
   constructor( private _http: Http ) { }
-	
   getAlbum(id: number){
-	  
-	  return this._http.get (this._albumUrl ).map((response) => response.json()) ;
+    return this._http.get(this._albumUrl ).map((response) => response.json()) ;
 	  
   }
 }
